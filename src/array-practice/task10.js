64a4d7dfcb74832620b3fa34370df0364b6c7a1c
nikita-m323 <nikitosina323@gmail.com>
2020-08-28 22:18:11 +0300

    array-practice completed task10

--- src/array-practice/task10.js
+++ src/array-practice/task10.js
@@ -10,11 +10,12 @@
 
 // Раскомментировать нижнее, при начале реализации
 
-// const meetups = [
-//   { name: 'JavaScript', isActive: true, members: 100 },
-//   { name: 'Angular', isActive: true, members: 900 },
-//   { name: 'Node', isActive: false, members: 600 },
-//   { name: 'React', isActive: true, members: 500 },
-// ];
-
-// function membersOnActiveMeetups(meetups) {}
+// eslint-disable-next-line import/prefer-default-export
+export function membersOnActiveMeetups(array) {
+  return array.reduce(function(sum, item) {
+    if (item.isActive === true) {
+      return sum + item.members;
+    }
+    return sum;
+  }, 0);
+}
