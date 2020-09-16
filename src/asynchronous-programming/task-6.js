2b4f825bfc2b33a23c323ff542a9c984cc50e673
nikita-m323 <nikitosina323@gmail.com>
2020-09-16 20:47:33 +0300

    async task-6 completed

---
+++ src/asynchronous-programming/task-6.js
@@ -0,0 +1,8 @@
+function getResolvePromice(value) {
+  return new Promise(resolve => {
+    resolve(value);
+  });
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export { getResolvePromice };
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { parseJSON, successCb, failureCb } from './asynchronous-programming/task
 import { deley } from './asynchronous-programming/task-3';
 import { getData } from './asynchronous-programming/task-4';
 
+import { getResolvePromice } from './asynchronous-programming/task-6';
+
 foo(5, createCb(7));
 foo(20, createCb('cb'));
 
@@ -20,3 +22,16 @@ deley(1000).then(value => console.log(`Done with ${value}`));
 console.log('//////////////////////////');
 
 getData();
+
+console.log('//////////////////////////');
+
+getResolvePromice(500)
+  .then(response => {
+    if (response > 300) {
+      throw new Error('Ошибка');
+    }
+  })
+  .catch(e => console.log(e))
+  .finally(() => console.log('This is Finally'));
+
+console.log('//////////////////////////');
