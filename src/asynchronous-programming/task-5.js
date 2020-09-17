b57bee031626aac995751b1771c0f5db47191912
nikita-m323 <nikitosina323@gmail.com>
2020-09-18 00:49:45 +0300

    async task-5 completed

--- src/asynchronous-programming/task-5.js
+++ src/asynchronous-programming/task-5.js
@@ -4,21 +4,32 @@ const c = fetch('http://www.json-generator.com/api/json/get/cfDZdmxnDm');
 const d = fetch('http://www.json-generator.com/api/json/get/cfkrfOjrfS');
 const e = fetch('http://www.json-generator.com/api/json/get/ceQMMKpidK');
 
-function parallel() {
+/* function parallel() {
   const promises = [a, b, c, d, e];
-  Promise.all(
-    promises.map(request => {
-      return fetch(request)
-        .then(response => {
-          return response.json();
-        })
-        .then(data => {
-          return data;
-        });
-    })
-  ).then(values => {
-    console.log('values', values);
+  Promise.all(promises)
+    .then(responses => Promise.all(responses.map(r => r.json())))
+    .then(data => {
+      console.log(data);
+    });
+} 
+
+parallel(); */
+
+function methodThatReturnsAPromise(id) {
+  return new Promise(resolve => {
+    id.then(response =>
+      response.json().then(data => {
+        console.log(data);
+        resolve(data);
+      })
+    );
   });
 }
+const promises = [a, b, c, d, e];
+const result = promises.reduce((accumulatorPromise, nextID) => {
+  return accumulatorPromise.then(() => {
+    return methodThatReturnsAPromise(nextID);
+  });
+}, Promise.resolve());
 
-parallel();
+result.then(item => item);
